1476f69598dae5306d93941b65444c7b21421eb8
RoyLin <1002591652@qq.com>
2024-12-08 13:55:29 +0800

    feat: 网关配置

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
 	res.send(`Pod IP is ${process.env.POD_IP}`);
 });
 
+app.get('/data', (req, res) => {
+	res.json({
+		ip: process.env.POD_IP
+	});
+});
+
 app.listen(3000, () => {
 	console.log("Server started on port 3000");
 });
